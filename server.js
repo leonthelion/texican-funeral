c7cf141605360aa7a0cf93e4e4eee92131f26816
Luca Junk <lucaleonjunk@web.de>
2013-12-23 15:18:52 +0100

    fixed sort order and added initdb route

--- server.js
+++ server.js
@@ -56,7 +56,7 @@ server.get('/', function(req, res){
 		}
 		
 		res.render('index', {
-			posts: rows.reverse()
+			posts: rows.sort(function(a,b) {return (a.postid > b.postid) ? 1 : ((b.postid > a.postid) ? -1 : 0);} ).reverse()
 		});
 	});
 });
@@ -143,7 +143,7 @@ server.get('/admin', function(req, res){
 		}
 		
 		res.render('admin/index', {
-			posts: rows.reverse()
+			posts: rows.sort(function(a,b) {return (a.postid > b.postid) ? 1 : ((b.postid > a.postid) ? -1 : 0);} ).reverse()
 		});
 	});
 });
@@ -220,6 +220,23 @@ server.del('/admin/image/:id', function(req, res){
 	});
 });
 
+
+//route initdb
+server.get('/initdb', function(req, res){
+	query("CREATE TABLE posts ( postid serial NOT NULL, title character varying(255), content character varying(10000), day integer, month integer, year integer, CONSTRAINT postid PRIMARY KEY (postid) ) WITH ( OIDS=FALSE ); ALTER TABLE posts OWNER TO test;", function(err){
+		if (err) throw err;
+		query("CREATE TABLE sessions ( sid character varying(255) NOT NULL, username character varying(255), csrf character varying(255), login timestamp without time zone, logout timestamp without time zone, CONSTRAINT sid PRIMARY KEY (sid) ) WITH ( OIDS=FALSE ); ALTER TABLE sessions OWNER TO test;", function(err){
+			if (err) throw err;
+			query("CREATE TABLE users ( id serial NOT NULL, username character varying(255), password character varying(255), CONSTRAINT id PRIMARY KEY (id) ) WITH ( OIDS=FALSE ); ALTER TABLE users OWNER TO test;", function(err){
+				if (err) throw err;
+				query("CREATE TABLE images ( id serial NOT NULL, path character varying(255), name character varying(255), CONSTRAINT images_pkey PRIMARY KEY (id) ) WITH ( OIDS=FALSE ); ALTER TABLE images OWNER TO test;", function(err){
+					if (err) throw err;
+					res.redirect('/');
+				});
+			});
+		});
+	});
+});
 
 
 
